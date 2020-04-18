aa0f4bb0f9515cfb4b29ebe0ca25a8642f3c3a4a
Matthijs Hartskeerl <matthijs@hartskeerl.nl>
2020-04-18 10:53:25 +0200

    Added items

---
+++ csOpenGL/Balance/Items/Sword.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LD46
+{
+    class Sword : Item
+    {
+        public Sword() : base(
+            "Sword", 
+            Balance.SWORD_RARITY, 
+            Balance.SWORD_DESCRIPTION, 
+            new Sprite(0,0,0,new Texture("",0,0,0,0)),
+            new Effect[] { new Effect(EffectType.PHYSICAL_DAMAGE, 0.1, -999) }
+            )
+        {
+
+        }
+    }
+}
---
+++ csOpenGL/Effect.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LD46
+{
+    public class Effect
+    {
+        public EffectType Affects { get; set; }
+        public double Modifier { get; set; } //You can cast this to int if you need to
+        public double TimeLeft { get; set; }
+
+        public Effect(EffectType affects, double modifier, double timeLeft)
+        {
+            Affects = affects;
+            Modifier = modifier;
+            TimeLeft = timeLeft;
+        }
+
+        public bool HasExpired (double deltaTime)
+        {
+            if(TimeLeft == -999) //-999 means doesn't expire
+            {
+                return false; // Therefor it never expires
+            } else if(TimeLeft < 0) // Had already expired before
+            {
+                return true; //So we don't want to decrease the timeleft even further
+            }
+            TimeLeft -= deltaTime;
+            return TimeLeft < 0;
+        }
+    }
+
+    public enum EffectType
+    {
+        HP,
+        PHYSICAL_DAMAGE,
+        MAGICAL_DAMAGE,
+        BLOCK,
+    }
+}
