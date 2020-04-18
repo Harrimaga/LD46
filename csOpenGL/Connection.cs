f1de04fcfa878066f9d3c6f81a854184ba258f33
Matthijs Hartskeerl <matthijs@hartskeerl.nl>
2020-04-18 15:35:49 +0200

    YAYAYAYAYAYAY

---
+++ csOpenGL/Connection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LD46
+{
+    public class Connection
+    {
+        public Room Room { get; set; }
+        public Direction Direction { get; set; }
+        public int location { get; set; }
+
+        public Connection(Room room, Direction direction, int location)
+        {
+            Room = room;
+            Direction = direction;
+            this.location = location;
+        }
+    }
+}
