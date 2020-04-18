909c663b1fb3dbf38059b6ede06b453c80e77932
ilan hartskeerl <ilan@hartskeerl.nl>
2020-04-18 19:13:18 +0200

    Fixed minimap, AGAIN

--- csOpenGL/Fighter.cs
+++ csOpenGL/Fighter.cs
@@ -9,7 +9,7 @@ namespace LD46
     public class Fighter : Player
     {
 
-        public Fighter(float x, float y) : base(Balance.FIGHTER_BASE_HEALTH, x, y, 0, 3, 1, (int)(Globals.TileSize*0.9), (int)(Globals.TileSize*0.9), Balance.FIGHTER_BASE_SPEED, Balance.FIGHTER_BASE_ATTACK_POINT, Balance.FIGHTER_BASE_ATTACK_SPEED, Balance.FIGHTER_NAME, Balance.FIGHTER_BASE_DAMAGE)
+        public Fighter(float x, float y) : base(Balance.FIGHTER_BASE_HEALTH, x, y, 0, 3, 1, (int)(Globals.TileSize*0.85), (int)(Globals.TileSize*0.85), Balance.FIGHTER_BASE_SPEED, Balance.FIGHTER_BASE_ATTACK_POINT, Balance.FIGHTER_BASE_ATTACK_SPEED, Balance.FIGHTER_NAME, Balance.FIGHTER_BASE_DAMAGE)
         {
 
         }
--- csOpenGL/Player.cs
+++ csOpenGL/Player.cs
@@ -39,6 +39,12 @@ namespace LD46
 
         public override void Update(double delta)
         {
+            float dis = (float)Math.Sqrt(xDir * xDir + yDir * yDir);
+            if(dis != 0)
+            {
+                xDir /= dis;
+                yDir /= dis;
+            }
             Move((float)(delta * xDir * speed), (float)(delta * yDir * speed));
 
             for (int i = (int)(x / Globals.TileSize); i < (int)(x / Globals.TileSize) + 2 + w / Globals.TileSize && i < Globals.l.Current.width && i > -1; i++)
