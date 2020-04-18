3bfd8423b9f3d7b35773297a67dc23f489b7a25f
ilan hartskeerl <ilan@hartskeerl.nl>
2020-04-18 21:10:56 +0200

    FIX

--- csOpenGL/Items/Sword.cs
+++ csOpenGL/Items/Sword.cs
@@ -9,11 +9,14 @@ namespace LD46
     public class Sword : Item
     {
         public Sword() : base(
-            "Sword", 
-            Balance.SWORD_RARITY, 
-            Balance.SWORD_DESCRIPTION, 
-            new Sprite(40,40,0,Window.texs[4]),
+            "Sword",
+            Balance.SWORD_RARITY,
+            Balance.SWORD_DESCRIPTION,
+            new Sprite(40, 40, 0, Window.texs[4]),
             new Effect[] { new Effect(EffectType.PHYSICAL_DAMAGE, 0.1, -999) }
             )
+        {
+
+        }
     }
 }
