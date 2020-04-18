a39555bc583ad17ed4af4afe41040158575a8f5b
ilan hartskeerl <ilan@hartskeerl.nl>
2020-04-18 11:01:11 +0200

    fix

--- csOpenGL/Game.cs
+++ csOpenGL/Game.cs
@@ -34,13 +34,10 @@ namespace LD46
         public void Update(double delta)
         {
             //Updating logic
-            if (left.IsDown())
-            {
-                Window.camX -= (float)(10 * delta);
-            }
-            if (right.IsDown()) Window.camX += (float)(10 * delta);
-            if (up.IsDown()) Window.camY -= (float)(10 * delta);
-            if (down.IsDown()) Window.camY += (float)(10 * delta);
+            if (left.IsDown()) p.SetDir(-1, 0);
+            if (right.IsDown()) p.SetDir(1, 0);
+            if (up.IsDown()) p.SetDir(0, -1);
+            if (down.IsDown()) p.SetDir(0, 1);
 
             p.Update(delta);
 
@@ -49,9 +46,9 @@ namespace LD46
         public void Draw()
         {
             //Do all you draw calls here
+            room.Draw(0, 0);
             p.Draw();
             e.Draw();
-            room.Draw(0, 0);
         }
 
         public void MouseDown(MouseButtonEventArgs e, int mx, int my)
