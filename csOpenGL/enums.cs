6510571b3b59fba36f9f6e72d1e91e7fadbf7a72
Matthijs Hartskeerl <matthijs@hartskeerl.nl>
2020-04-18 11:13:01 +0200

    Enums

---
+++ csOpenGL/enums.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LD46
+{
+    enum Rarity
+    {
+        BASIC, //Starting items
+        COMMON,
+        UNCOMMON,
+        RARE,
+        SUPER_RARE
+    }
+
+    public enum EffectType
+    {
+        HP,
+        PHYSICAL_DAMAGE,
+        MAGICAL_DAMAGE,
+        BLOCK,
+    }
+}
