1b145d757932e941cc3fa35753418bb4557f6b23
KingTurtleTheFrog <timvanbrederode@live.nl>
2020-04-18 20:19:59 +0200

    ?

--- csOpenGL/Player.cs
+++ csOpenGL/Player.cs
@@ -203,7 +203,7 @@ namespace LD46
             HBarBackUI.w = 200;
             HBarBackUI.h = 30;
             HBarBackUI.Draw(1720, 750, 0, 0, 0, 0);
-            HBarUI.Draw(1720, 750, 0, (float)(1 - Health / MaxHealth) / 2, (float)(Health / MaxHealth) / 2, 0);
+            HBarUI.Draw(1720, 750, 0, (float)(1 - Health / MaxHealth) / 2, (float)(Health / MaxHealth) / 2, 0); 
 
             MBarUI.w = (int)(200 * Mana / MaxMana);
             MBarUI.h = 30;
