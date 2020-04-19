fc5f6d45d29da49d67971ecbb18d2346d0c3bf25
KingTurtleTheFrog <timvanbrederode@live.nl>
2020-04-19 21:00:03 +0200

    Re-added Hirrathak spawn, improved Pylonius damage

--- csOpenGL/Balance/Enemies.cs
+++ csOpenGL/Balance/Enemies.cs
@@ -71,7 +71,7 @@ namespace LD46
         public const double PYLONIUS_SPEED = 0.5;
         public const double PYLONIUS_ATTACKSPEED = 30;
         public const double PYLONIUS_ATTACKPOINT = 0.6;
-        public const double PYLONIUS_DAMAGE = 1;
+        public const double PYLONIUS_DAMAGE = 30;
         public const double PYLONIUS_BLOCK = 5;
         public const double PYLONIUS_PHYSICAL_AMP = 1.5;
         public const double PYLONIUS_MAGICAL_AMP = 0.3;
--- csOpenGL/Game.cs
+++ csOpenGL/Game.cs
@@ -194,7 +194,7 @@ namespace LD46
             buttons.Clear();
             pp.ReAddButtons();
             p = pp;
-            Globals.PossibleBosses = new List<Enemy> { /*new Hirrathak(),*/ new Pylonius() };
+            Globals.PossibleBosses = new List<Enemy> { new Hirrathak(), new Pylonius() };
             new Level(Globals.Themes[Globals.Rng.Next(Globals.Themes.Count)], p, Globals.Rng.Next());
             gameState = GameState.PLAYING;
             LevelsPlayed = 0;
