6ccff68f5a38895ef61c76011dbc247e6b8a9d8a
Matthijs Hartskeerl <matthijs@hartskeerl.nl>
2020-04-19 18:55:21 +0200

    wall

---
+++ csOpenGL/Structures/Wall.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LD46
+{
+    public class Wall : Structure
+    {
+        public Wall(): base(3,1)
+        {
+
+        }
+
+        public override void Place(int[,] tileGrid)
+        {
+            for(int i = X; i<X+Width; i++)
+            {
+                for(int j = Y; j<Y+Height; j++)
+                {
+                     tileGrid[i, j] = new Tile(new Sprite(Globals.TileSize, Globals.TileSize, 0, theme.GetTextureByType(TileType.WALL)), Walkable.SOLID, TileType.WALL, rotation);
+                }
+            }
+        }
+    }
+}
