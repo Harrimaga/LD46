56e872d63beb28a3d05e5abb77a70133c31cf7e1
Matthijs Hartskeerl <matthijs@hartskeerl.nl>
2020-04-20 21:21:44 +0200

    Names

--- csOpenGL/Enemies/DarkEnemy.cs
+++ csOpenGL/Enemies/DarkEnemy.cs
@@ -9,7 +9,7 @@ namespace LD46
     class DarkEnemy : Enemy
     {
 
-        public DarkEnemy(int x, int y) : base(Enemies.DARK_ENEMY_HEALTH, Enemies.DARK_ENEMY_MANA, x, y, 0, 3, 3, Globals.TileSize, Globals.TileSize, Enemies.DARK_ENEMY_SPEED, Enemies.DARK_ENEMY_ATTACKPOINT, Enemies.DARK_ENEMY_ATTACKSPEED, Enemies.DARK_ENEMY_DAMAGE, "Kees", Enemies.DARK_BLOCK)
+        public DarkEnemy(int x, int y) : base(Enemies.DARK_ENEMY_HEALTH, Enemies.DARK_ENEMY_MANA, x, y, 0, 3, 3, Globals.TileSize, Globals.TileSize, Enemies.DARK_ENEMY_SPEED, Enemies.DARK_ENEMY_ATTACKPOINT, Enemies.DARK_ENEMY_ATTACKSPEED, Enemies.DARK_ENEMY_DAMAGE, "a Dario", Enemies.DARK_BLOCK)
         {
             attackAni = new Animation(0, 9, 10);
             idleAni = new Animation(0, 3, 10);
